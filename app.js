9cb84790ce32c4cbdd683aafc8d8dd6068f57090
Tae Yun <yuntjs@gmail.com>
2017-03-11 18:35:22 -0500

    set up database and mongoose

--- app.js
+++ app.js
@@ -3,14 +3,34 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes')
-var jsonParser = require('body-parser').json;
 
+var jsonParser = require('body-parser').json;
+var logger = require('morgan');
+
+app.use(logger("dev"));
 app.use(jsonParser());
 
 app.use("/questions",routes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next){
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error Handler
+app.use(function(err,req,res,next){
+    res.status(err.status || 500);
+    res.json({
+      error: {
+        message: err.message
+      }
+    });
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
-  console.log("Express server is listening on post", port);
+    console.log("Express server is listening on post", port);
 });
--- routes.js
+++ routes.js
@@ -14,10 +14,51 @@ router.post("/", function(req,res){
   });
 });
 
-router.get("/:id", function(req,res){
+router.get("/:qID", function(req,res){
   res.json({
-    response: "You sent me a POST request for ID" + req.params.id
+    response: "You sent me a POST request for ID" + req.params.qID
   });
 });
 
+router.post("/:qID/answers", function(req,res){
+  res.json({
+    response: "You sent me a POST request to /answers",
+    questionId: req.params.qID,
+    body: req.body
+  });
+});
+
+router.put("/:qID/answers/:aID", function(req,res){
+  res.json({
+    response: "You sent me a PUT request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    body: req.body
+  });
+});
+
+router.delete("/:qID/answers/:aID", function(req,res){
+  res.json({
+    response: "You sent me a DELETE request to /answers",
+    questionId: req.params.qID,
+    answerId: req.params.aID
+  });
+});
+
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res, next){
+  if(req.params.dir.search(/^up|down$/) == -1){
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    next()
+  }
+},function(req,res){
+  res.json({
+    response: "You sent me a POST request to /vote-"+req.params.dir,
+    questionId: req.params.qID,
+    answerId: req.params.aID,
+    vote: req.params.dir
+  });
+});
 module.exports = router;
