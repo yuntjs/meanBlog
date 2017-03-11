b5ef899d3a1dbe6f0e7cbc8d03e980eaac4bf591
Tae Yun <yuntjs@gmail.com>
2017-03-11 15:40:56 -0500

    build server app

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var jsonParser = require('body-parser').json;
+
+app.use(jsonParser());
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log("Express server is listening on post", port);
+});
